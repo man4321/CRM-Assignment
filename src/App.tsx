f009bb9368b1e76066285241364be08f44b236d9
man4321 <manhar2000.seervi@gamil.com>
2025-08-02 11:24:44 +0530

    vite+ react-ts setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { JSX, useState } from 'react'
+import './App.css'
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Hello React + TypeScript!</h1>
+    </div>
+  )
+}
+
+export default App
